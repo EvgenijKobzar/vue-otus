e9cc82293201c952e2085131de5c832ca78315ab
EvgenijKobzar <9097963939@rambler.ru>
2023-01-12 18:09:25 +0200

    otus: init

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+library.add(faHatWizard)
+
+createApp(App).mount('#app')
