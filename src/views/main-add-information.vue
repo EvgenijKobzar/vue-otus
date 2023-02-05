08c37b7f66e73185068d3217164f6fcf773fa0ac
EvgenijKobzar <9097963939@rambler.ru>
2023-02-05 18:34:41 +0200

    дополнение по формам добавления

---
+++ src/views/main-add-information.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="col-xl-4 col-lg-5">
+		<div class="widget-title mb-50">
+			<h5 class="title">Информация</h5>
+		</div>
+		<div class="contact-info-wrap">
+			<p><span>После проверки</span> инфомрация о эпизодах и сезонах обновиться на сайте</p>
+
+		</div>
+	</div>
+</template>
+
+<script setup>
+</script>
--- src/views/main-season-add.vue
+++ src/views/main-season-add.vue
@@ -40,30 +40,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-xl-4 col-lg-5">
-					<div class="widget-title mb-50">
-						<h5 class="title">Information</h5>
-					</div>
-					<div class="contact-info-wrap">
-						<p><span>Find solutions :</span> to common problems, or get help from a support agent industry's standard .</p>
-						<div class="contact-info-list">
-							<ul>
-								<li>
-									<div class="icon"><i class="fas fa-map-marker-alt"></i></div>
-									<p><span>Address :</span> W38 Park Road New York</p>
-								</li>
-								<li>
-									<div class="icon"><i class="fas fa-phone-alt"></i></div>
-									<p><span>Phone :</span> (09) 123 854 365</p>
-								</li>
-								<li>
-									<div class="icon"><i class="fas fa-envelope"></i></div>
-									<p><span>Email :</span> support@movflx.com</p>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
+				<main-add-information/>
 			</div>
 		</div>
 	</section>
@@ -71,6 +48,7 @@
 
 <script setup>
 import {reactive} from "vue";
+import MainAddInformation from "./main-add-information.vue";
 
 const state = reactive({
 	fields: {
