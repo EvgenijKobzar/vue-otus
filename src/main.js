8dc6d09a087a0b262098a754f391c80af48f136f
EvgenijKobzar <9097963939@rambler.ru>
2023-02-05 20:57:44 +0200

    удалил лишнее

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-
-
 import Application from "./application/application.js";
 new Application('cinema-app');
-// createApp(App).mount('#app')
 
 
